d48a8d7e085a82290bcb0c39afde85d524a81208
Taras Burdeniuk <bur.taras@gmail.com>
2019-12-04 22:08:01 +0200

    connected eslint

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -4,39 +4,15 @@ import NavBar from './components/NavBar';
 import THEME from './theme';
 import MainScreen from './Screens/MainScreen';
 import TodoScreen from './Screens/TodoScreen';
-import TodoContext from './context/todo/todoContext';
 import ScreenContext from './context/screen/screenContext';
 
 const MainLayout = () => {
-    const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext);
-    const { todoId, changeScreen } = useContext(ScreenContext);
-    // const [todoId, setTodoId] = useState(null);
-
-    let content = (
-        <MainScreen
-            removeTodo={removeTodo}
-            todos={todos}
-            addTodo={addTodo}
-            openTodo={changeScreen}
-        />
-    );
-
-    if (todoId) {
-        const selectedTodo = todos.find(item => item.id === todoId);
-        content = (
-            <TodoScreen
-                onRemove={removeTodo}
-                goBack={() => changeScreen(null)}
-                todo={selectedTodo}
-                onSave={updateTodo}
-            />
-        );
-    }
+    const { todoId } = useContext(ScreenContext);
 
     return (
         <View>
             <NavBar title="Todo App" />
-            <View style={styles.container}>{content}</View>
+            <View style={styles.container}>{todoId ? <TodoScreen /> : <MainScreen />}</View>
         </View>
     );
 };
